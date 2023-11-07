c57be2bfb767367345cce6428d77af360d30393c
mongoq <mongoq@das-labor.org>
2023-11-07 22:21:06 +0100

    Update classify1.py

--- Teachable-Machine/classify1.py
+++ Teachable-Machine/classify1.py
@@ -2,14 +2,11 @@ import tensorflow as tf
 import numpy as np
 import cv2
 
-# Lade das Teachable Machine-Modell aus der H5-Datei
 model = tf.keras.models.load_model('keras_model.h5')
 
-# Lade die Labels aus der Labels-Datei
 with open('labels.txt', 'r') as file:
     labels = file.read().splitlines()
 
-# Funktion zur Klassifikation
 def classify_image(image):
     img = cv2.resize(image, (224, 224))
     img = tf.image.convert_image_dtype(img, tf.float32)
@@ -21,7 +18,6 @@ def classify_image(image):
 
     return labels[predicted_class], confidence
 
-# Webcam öffnen
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -31,10 +27,10 @@ while True:
         break
 
     class_name, confidence = classify_image(frame)
-    cv2.putText(frame, f'Klasse: {class_name}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.putText(frame, f'Zuversicht: {confidence * 100:.2f}%', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame, f'Class: {class_name}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame, f'Confidence: {confidence * 100:.2f}%', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    cv2.imshow('Webcam Klassifikation', frame)
+    cv2.imshow('Webcam Classification', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
